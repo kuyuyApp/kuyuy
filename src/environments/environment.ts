0fa2944a76b6002b126f004cfc079103cfda45ea
Denix7 <dennis.brian.pv@gmail.com>
2020-05-13 18:38:27 -0400

    Instalando dependencias de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyBahocxKp-n3hj5fqM-OHRVP4SqnIjinbs",
+    authDomain: "kuyuy-a261e.firebaseapp.com",
+    databaseURL: "https://kuyuy-a261e.firebaseio.com",
+    projectId: "kuyuy-a261e",
+    storageBucket: "kuyuy-a261e.appspot.com",
+    messagingSenderId: "789473878230",
+    appId: "1:789473878230:web:29995c34f263b1f5162977",
+    measurementId: "G-9FS4G0K9F0"
 };
 
 /*
